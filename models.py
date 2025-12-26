b136373337cd7eecb029776f0356d22b32d6b794
noreildenuno <noreildenuno@gmail.com>
2025-12-26 12:26:56 +0100

    Voeg Speler klasse toe met attributen en __repr__ methode

---
+++ models.py
@@ -0,0 +1,11 @@
+class Speler:
+    def __init__(self, id, naam, leeftijd, club, positie_id, waarde):
+        self.id = id
+        self.naam = naam
+        self.leeftijd = leeftijd
+        self.club = club
+        self.positie_id = positie_id
+        self.waarde = waarde
+
+    def __repr__(self):
+        return f"Speler({self.id}, '{self.naam}', {self.leeftijd}, '{self.club}', positie_id={self.positie_id}, waarde={self.waarde})"
