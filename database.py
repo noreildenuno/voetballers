5f66a64d07cd7accb09437497869b4c574b1973c
noreildenuno <noreildenuno@gmail.com>
2025-12-26 11:52:23 +0100

    tabel posities toevoegen in database

--- database.py
+++ database.py
@@ -6,3 +6,14 @@ class Database:
     def __init__(self, db_pad="data/voetballers.db"):
         Path("data").mkdir(exist_ok=True)
         self.verbinding = sqlite3.connect(db_pad)
+        self.maken_tabellen()
+
+    def maken_tabellen(self):
+        cursor = self.verbinding.cursor()
+        cursor.execute("""
+            CREATE TABLE IF NOT EXISTS posities (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                naam TEXT NOT NULL
+            )
+        """)
+        self.verbinding.commit()
