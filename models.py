918f14913548fd45b91d2cea676aaae740dd50b1
noreildenuno <noreildenuno@gmail.com>
2025-12-26 12:45:29 +0100

    Ik maak hier een klasse positie en voeg attributen toe

--- models.py
+++ models.py
@@ -9,3 +9,11 @@ class Speler:
 
     def __repr__(self):
         return f"Speler({self.id}, '{self.naam}', {self.leeftijd}, '{self.club}', positie_id={self.positie_id}, waarde={self.waarde})"
+    
+class Positie:
+    def __init__(self, id, naam):
+        self.id = id
+        self.naam = naam
+
+    def __repr__(self):
+        return f"Positie({self.id}, '{self.naam}')"
